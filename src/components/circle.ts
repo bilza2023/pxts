01e4854581696002907871f86453e39d22389526
unknown <bilza32611@gmail.com>
2022-12-07 11:49:02 +0500

    dat.gui added

--- src/components/circle.ts
+++ src/components/circle.ts
@@ -12,7 +12,7 @@ export default class Ellipse extends BaseGraph {
     //xxxxxxx
     init() {
         this.graphics.beginFill(0xffffff); // Red
-        this.graphics.drawEllipse(0, 0, this.width, this.height); // drawCircle(x, y, radius)
+        this.graphics.drawCircle(0, 0, this.width); // drawCircle(x, y, radius)
         this.graphics.endFill();
     }
 
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import { Graphics, Container, DisplayObject, Application } from "pixi.js";
 import PixiEngine from "./pixiEngine";
 import Rect from "./components/rect";
 import Ellipse from "./components/ellipse";
+import RoundRect from "./components/roundRect";
+import * as dat from "dat.gui";
+
 ////////////////////////////////////////////////
 const canvasWidth = 800;
 const canvasHeight = 300;
@@ -25,7 +28,6 @@ rect.init();
 rect.pivot(1, 1);
 
 rect.update();
-
 //nnnnnnnnnnnnnnnnnnnnnnnnnnnnnn
 const c = new Ellipse(0, 0, 50, 50, 0x00ffff);
 c.init();
@@ -36,22 +38,39 @@ c.y = 100;
 // c.getDrawable().tint = 0x245399;
 
 c.update();
-
+//.............................
+const rr = new RoundRect(500, 20, 100, 50, 0xff0000, 10);
+rr.init();
+engine.add(rr.getDrawable());
+rr.color = 0x0000ff;
+rr.update();
 //////////////////////////
 setInterval(function () {
-    rect.angle += 0.5;
-    if (rect.angle > 10) {
-        rect.color = 0x245399;
-    }
+    // rect.angle += 0.5;
+    // if (rect.angle > 10) {
+    //     rect.color = 0x245399;
+    // }
     rect.update();
 
     ////////////////////////////////
-    c.x += 0.3;
-    c.width += 0.3;
-    c.height += 0.3;
+    // c.x += 0.3;
+    // c.width += 0.3;
+    // c.height += 0.3;
     // c.opacity -= 0.1;
-    if (c.width > 70) {
-        c.color = 0xff0000;
-    }
+    // if (c.width > 70) {
+    //     c.color = 0xff0000;
+    // }
     c.update();
+
+    ////
+    // rr.width += 0.3;
+    // rr.height += 0.3;
+    rr.update();
 }, 20);
+
+//////////////////////
+
+const gui = new dat.GUI();
+gui.add(c, "width", 1, 500);
+gui.add(c, "x", 0, 800);
+gui.add(c, "y", 0, 300);
