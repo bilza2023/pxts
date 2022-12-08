b8714574ec7252e511d994c68692174091d56e4e
unknown <bilza32611@gmail.com>
2022-12-08 19:37:18 +0500

    first test production build created with in archive folder - and samples created based on that

---
+++ src/libs/rect/main.js
@@ -0,0 +1,7 @@
+import { Graphics, Container, DisplayObject, Application } from "pixi.js";
+// import '@pixi/graphics-extras';
+// import PixiEngine from "./engine/pixiEngine";
+// import * as dat from "dat.gui";
+// import Rect from "./graphComps/rect";
+
+console.log("ddd");
