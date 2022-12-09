9d34c9d724a712ea0bd09aa94064989a9fb37734
unknown <bilza32611@gmail.com>
2022-12-10 00:37:56 +0500

    converted to vite and completed first sample using dat.gui externally

---
+++ vite.config.js
@@ -0,0 +1,34 @@
+import { defineConfig } from 'vite';
+import url from 'url';
+import path from 'path';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default defineConfig(({ command, mode }) => ({
+//   base: command === 'build' ? '/etc.clientlibs/<project>/clientlibs/' : '/',
+
+  publicDir: command === 'build' ? false : 'src/assets',
+
+  build: {
+    brotliSize: false,
+    manifest: false,
+    minify: mode === 'development' ? false : 'terser',
+    outDir: 'dist',
+    assetsDir : 'assets',
+    sourcemap: command === 'serve' ? 'inline' : false,
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'pxts',
+      formats : ['es'],
+      // the proper extensions will be added
+      fileName: 'pxts',
+    }
+
+  },
+
+  server: {
+    origin: 'http://localhost:3000',
+  },
+}));
